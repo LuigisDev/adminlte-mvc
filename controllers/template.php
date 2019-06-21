47673073b327f75cef88879ae783b04187bd0a4a
Luis García <luis_grdz@hotmail.com>
2019-06-20 21:49:39 -0500

    Configuración básica general de plantilla adminlte

---
+++ controllers/template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Luis Alberto García Rodríguez
+*/
+class templateController 
+{
+	function __construct()
+	{
+		
+	}
+
+	public function template()
+	{
+		include ("views/template.php");
+	}
+}
+
+?>
